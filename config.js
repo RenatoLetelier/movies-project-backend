db0ce262f12cfe461791ec556c30d41a9452da87
RenatoLetelier <renato.letelier@gmail.com>
2025-04-18 16:00:18 -0400

    little fixes

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
+  HOST: process.env.SERVER_HOST || 'localhost',
+  PORT: process.env.SERVER_PORT || '8000',
+  JWT_SECRET: process.env.JWT_SECRET,
   DB_TYPE: process.env.DB_TYPE || 'mysql', // 'mongodb' o 'mysql'
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_MOVIES_NAME: process.env.DB_MOVIES_NAME,
+  DB_PHOTOS_NAME: process.env.DB_PHOTOS_NAME
 };
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const config = require('./config');
 
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
+// const fs = require('fs');
+// const ffmpeg = require('fluent-ffmpeg');
+// const path = require('path');
 
 // const router = express.Router();
 
@@ -15,10 +16,6 @@ const moviesRoutes = require('./routes/moviesRoutes');
 const photosRoutes = require('./routes/photosRoutes');
 
 const app = express();
-
-const HOST = process.env.SERVER_HOST;
-const PORT = process.env.SERVER_PORT;
-const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
@@ -32,7 +29,7 @@ function verificarToken(req, res, next) {
     const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ error: 'Acceso denegado' });
   
-    jwt.verify(token.split(' ')[1], JWT_SECRET, (err, decoded) => {
+    jwt.verify(token.split(' ')[1], config.JWT_SECRET, (err, decoded) => {
       if (err) return res.status(401).json({ error: 'Token inválido' });
   
       req.user = decoded;
@@ -45,58 +42,61 @@ app.get('/', (req, res) => {
     res.send('Home server working correctly!');
 });
 
+app.get('*', (req, res) => {
+    res.send('Error page, please come back /home.');
+});
+
 // 📌 Ruta protegida (requiere token)
-app.get('/profile', verificarToken, (req, res) => {
-    res.json({ message: 'Bienvenido al perfil protegido', user: req.user });
-});
+// app.get('/profile', verificarToken, (req, res) => {
+//     res.json({ message: 'Bienvenido al perfil protegido', user: req.user });
+// });
 
-app.get('/movies/muxed/:id', (req, res) => {
-    const movieId = req.params.id;
-    const videoFile = "video";
-    const audioFile = "audio";
+// app.get('/movies/muxed/:id', (req, res) => {
+//     const movieId = req.params.id;
+//     const videoFile = "video";
+//     const audioFile = "audio";
     
-    const videoPath = path.join(__dirname, `./uploads/${videoFile}.mp4`);
-    const audioPath = path.join(__dirname, `./uploads/${audioFile}${movieId}.mp3`);
-    const muxedPath = path.join(__dirname, `./uploads/muxed-${movieId}.mp4`);
+//     const videoPath = path.join(__dirname, `./uploads/${videoFile}.mp4`);
+//     const audioPath = path.join(__dirname, `./uploads/${audioFile}${movieId}.mp3`);
+//     const muxedPath = path.join(__dirname, `./uploads/muxed-${movieId}.mp4`);
   
-    if (!fs.existsSync(videoPath) || !fs.existsSync(audioPath)) {
-      return res.status(404).send('Video or audio file not found');
-    }
+//     if (!fs.existsSync(videoPath) || !fs.existsSync(audioPath)) {
+//       return res.status(404).send('Video or audio file not found');
+//     }
 
-    // Si el archivo ya está muxeado, lo servimos directamente
-    if (fs.existsSync(muxedPath)) {
-        return res.sendFile(muxedPath);
-    }
+//     // Si el archivo ya está muxeado, lo servimos directamente
+//     if (fs.existsSync(muxedPath)) {
+//         return res.sendFile(muxedPath);
+//     }
   
-    res.contentType('video/mp4');
+//     res.contentType('video/mp4');
   
-    ffmpeg()
-    .input(videoPath)
-    .input(audioPath)
-    .outputOptions([
-      '-map 0:v:0',
-      '-map 1:a:0',
-      '-c:v copy',
-      '-c:a aac',
-      '-b:a 192k',
-      '-movflags frag_keyframe+empty_moov' // para compatibilidad con streaming
-    ])
-    .on('start', cmd => {
-      console.log('Muxing command:', cmd);
-    })
-    .on('end', () => {
-      console.log('Muxing completed.');
-      res.sendFile(muxedPath);
-    })
-    .on('error', err => {
-      console.error('FFmpeg error:', err);
-      if (!res.headersSent) res.status(500).send('Error muxing media');
-    })
-    .save(muxedPath);
-
-});
+//     ffmpeg()
+//     .input(videoPath)
+//     .input(audioPath)
+//     .outputOptions([
+//       '-map 0:v:0',
+//       '-map 1:a:0',
+//       '-c:v copy',
+//       '-c:a aac',
+//       '-b:a 192k',
+//       '-movflags frag_keyframe+empty_moov' // para compatibilidad con streaming
+//     ])
+//     .on('start', cmd => {
+//       console.log('Muxing command:', cmd);
+//     })
+//     .on('end', () => {
+//       console.log('Muxing completed.');
+//       res.sendFile(muxedPath);
+//     })
+//     .on('error', err => {
+//       console.error('FFmpeg error:', err);
+//       if (!res.headersSent) res.status(500).send('Error muxing media');
+//     })
+//     .save(muxedPath);
+// });
 
 // Iniciar el servidor
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Servidor funcionando en http://${HOST}:${PORT}`);
+app.listen(config.PORT, '0.0.0.0', () => {
+    console.log(`✅ Servidor funcionando en http://${config.HOST}:${config.PORT}`);
 });
